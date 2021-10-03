ebbde47085e304f9c258deab789e2a68e8307c7b
unknown <lalitasng1534@gmail.com>
2021-10-03 11:34:56 +0530

    copy string programme

---
+++ strings/copyString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1="Lalita kumari";
+    string s2="";
+    int i;
+    for( i=0;i<s1.length();i++){
+        s2+=s1[i];
+    } 
+    cout<<s2;
+
+    return 0;
+}
+
